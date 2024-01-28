46c281805be6d6a06497defaf307356eeb74ea8f
Cezar <marinacezar72@gmail.com>
2024-01-28 14:38:42 +0200

    border-box 1

--- src/App.js
+++ src/App.js
@@ -2,41 +2,137 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Clarifai from 'clarifai'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg'
 import { useState } from 'react';
 
-const app = new Clarifai.App({
-  apiKey: '3a1505c09d9e4b029ca6a33b50a75793'
-}); 
+
+function setupclarifai(imageUrl) {
+  const PAT = 'ec3d77d6047b49c98c552654b7781c44';
+  const USER_ID = '1bhpznnjeiuj';
+  const APP_ID = 'test';
+  const MODEL_ID = 'face-detection';
+  const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+  const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+  
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+  
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return { requestOptions, MODEL_ID, MODEL_VERSION_ID };
+}
 
 function App() {
-  const [inputValue, setInputValue] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
+  const [inputValue, setInputValue] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const { requestOptions, MODEL_ID, MODEL_VERSION_ID } = setupclarifai();
+  const [box,setBox] = useState({})
 
-  const onInputChange = (e) =>{
-    setInputValue(e.target.value)
+  /* const calculateFaceLocation = (data) =>{
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputimage')
+    debugger
+    const width = Number(image.width)
+    const height = Number(image.height)
+    console.log(width,height,clarifaiFace)
+  } */
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    /* return {
+      leftCol: (clarifaiFace.left_col * width).toFixed(3),
+      topRow: (clarifaiFace.top_row * height).toFixed(3),
+      rightCol: (width - (clarifaiFace.right_col * width)).toFixed(3),
+      bottomRow: (height - (clarifaiFace.bottom_row * height)).toFixed(3)
+    } */
+    const leftCol = (clarifaiFace.left_col * width).toFixed(3)
+    const topRow = (clarifaiFace.top_row * height).toFixed(3)
+    const rightCol = (width - (clarifaiFace.right_col * width)).toFixed(3)
+    const bottomRow = (height - (clarifaiFace.bottom_row * height)).toFixed(3)
+    
+    setBox({
+      leftCol: leftCol,
+      topRow: topRow,
+      rightCol: rightCol,
+      bottomRow: bottomRow
+    })
   }
-  const onButtonSubmit = () =>{
 
-    app.models
-      .predict(
-        'face-detection',
-        inputValue
-      )
-      .then(
-        setImageUrl(inputValue),
-        function(response) {
-          console.log(response)
-        },
-        function(err){
-          //there was an error
-        }
-      )
+  /* const displayFaceBox = (box) => {
+    setBox({box: box}); 
+  } */
+  
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value);
   }
 
+  const onButtonSubmit = () => {
+    if (!inputValue) {
+      console.log("Please enter a valid input value.");
+      return;
+    }
+    setImageUrl(inputValue);
+
+
+    fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`, requestOptions)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(result => {
+      calculateFaceLocation(result)
+      /* const regions = result.outputs[0].data.regions;
+      
+      regions.forEach(region => {
+        const boundingBox = region.region_info.bounding_box;
+        const topRow = boundingBox.top_row.toFixed(3);
+        const leftCol = boundingBox.left_col.toFixed(3);
+        const bottomRow = boundingBox.bottom_row.toFixed(3);
+        const rightCol = boundingBox.right_col.toFixed(3);
+        setBox({
+          leftCol: leftCol,
+          topRow: topRow,
+          rightCol: rightCol,
+          bottomRow: bottomRow
+        })
+      }); */
+  
+
+
+
+
+
+    })
+    .catch(error => console.error('Error:', error));
+  
+  };
 
   return (
     <div className="App">
@@ -45,9 +141,9 @@ function App() {
       <Logo />
       <Rank />
       <ImageLinkForm  onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
-      <FaceRecognition  imageUrl={imageUrl}/>
+      <FaceRecognition box = {box} imageUrl={imageUrl}/>
     </div>
   );
-}
+  }
 
 export default App;
--- src/components/FaceRecognition/FaceRecognition.jsx
+++ src/components/FaceRecognition/FaceRecognition.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
-  return (
-    <div className="center ma">
+const FaceRecognition = ({imageUrl, box}) => {
+  if(imageUrl) {
+    console.log(box.topRow)
+    return (
+      <div className='center ma'>
         <div className='absolute mt2'>
-        <img src={imageUrl} alt="something" width= '500px' height='auto'/>
+          <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
+          <div className='bounding-box' style={{top: `${box.topRow}px`, right: `${box.rightCol}px`, left: `${box.leftCol}px`, bottom: `${box.bottomRow}px`}}></div>
         </div>
-    </div>
-  )
+      </div>
+    );
+  } else {
+    return null; // Render nothing if imageUrl is falsy
+  }
 }
 
-export default FaceRecognition
+export default FaceRecognition;
